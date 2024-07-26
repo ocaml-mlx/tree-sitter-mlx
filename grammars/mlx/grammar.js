fbe904bbf55be15d34b57ba9c45750fc5187960f
Andrey Popp <8mayday@gmail.com>
2024-07-26 10:22:57 +0400

    Fix parsing JSX with module names as tags

--- grammars/mlx/grammar.js
+++ grammars/mlx/grammar.js
@@ -14,23 +14,30 @@ module.exports = grammar(require('../ocaml/grammar'), {
        $.jsx_element_self_closing,
        $._jsx_element
      ),
-    _jsx_element: ($) => seq(
+    _jsx_element: $ => seq(
       $.jsx_element_opening,
       repeat($._simple_expression),
       $.jsx_element_closing,
     ),
 
-    jsx_element_self_closing: ($) =>
-      seq('<', field('name', $.value_path), repeat($.jsx_prop), '/>'),
+    jsx_tag: $ =>
+      path($.module_path, choice($._value_name, $._module_name)),
 
-    jsx_element_opening: ($) =>
-      seq('<', field('name', $.value_path), repeat($.jsx_prop), '>'),
+    jsx_element_self_closing: $ =>
+      seq('<', $.jsx_tag, repeat($.jsx_prop), '/>'),
 
-    jsx_element_closing: ($) =>
-      seq('</', field('name', $.value_path), '>'),
+    jsx_element_opening: $ =>
+      seq('<', $.jsx_tag, repeat($.jsx_prop), '>'),
+
+    jsx_element_closing: $ =>
+      seq('</', $.jsx_tag, '>'),
 
     jsx_prop:       $ => seq($.jsx_prop_name, '=', $.jsx_prop_value),
-    jsx_prop_name:  ($) => seq(optional('?'), $._label_name),
-    jsx_prop_value: ($) => $._simple_expression,
+    jsx_prop_name:  $ => seq(optional('?'), $._label_name),
+    jsx_prop_value: $ => $._simple_expression,
   }
 })
+
+function path(prefix, final) {
+  return choice(final, seq(prefix, '.', final))
+}
